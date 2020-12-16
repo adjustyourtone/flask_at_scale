55a56d5402d3bfef6ae2885a7be8d4f989c43a33
Tony Rizzotto <anthonyrizzotto@gmail.com>
2020-12-16 11:04:46 -0500

    Add: defined initial routes

--- app/routes.py
+++ app/routes.py
@@ -13,17 +13,36 @@ def index():
 
 
 # Define a route to get Actors (/actors)
+@app.route('/actor')
+def get_actors():
+    pass
+
 
 # Define a route to get Movies (/movies)
+@app.route('/movies')
+def get_movies():
+    pass
+
 
 # Define a route to Create Actors (/actors)
+@app.route('/actors', methods=['POST'])
+def create_actor():
+    pass
+
+
+# define a route that can Patch and Delete actor by ID
+@app.route('/actors/<int:id>', methods=['PATCH', 'DELETE'])
+def update_actors(id):
+    pass
+
 
 # Define a route to Create Movies (/movies)
+@app.route('/movies', methods=['POST'])
+def create_movies():
+    pass
 
-# Define aroute to Patch Actors (/actors/<id>)
 
-# Define a route to Patch Movies (/movies/<id>)
-
-# Define a route to Delete (/actors/<id>)
-
-# Define a route to Delete movies (/movies/<id>)
+# Define a route to Patch and Update movies by ID
+@app.route('/movies/<int:id>', methods=['PATCH', 'DELETE'])
+def update_movies(id):
+    pass
